07f71fffaec5f6ca25442daaca8ac492298a98de
Peter Arwanitis <peter.arwanitis@cognite.com>
2023-10-29 13:12:56 +0100

    fx: deploy - changing the processing   - which wasn't working when `naming-pattern` where used   - using ExtractionPipelineList and `as_external_ids()` now

--- src/extpipes/commands/deploy.py
+++ src/extpipes/commands/deploy.py
@@ -1,6 +1,10 @@
 import logging
 
-from cognite.client.data_classes import ExtractionPipeline, ExtractionPipelineContact
+from cognite.client.data_classes import (
+    ExtractionPipeline,
+    ExtractionPipelineContact,
+    ExtractionPipelineList,
+)
 from jinja2 import Template
 
 from .base import CommandBase
@@ -18,64 +22,75 @@ def _render_template(template: str, metadata: dict[str, str]) -> str:
 class CommandDeploy(CommandBase):
     def command(self) -> None:
         # get existing extpipes
-        existing_extpipes = {ep.external_id: ep for ep in self.client.extraction_pipelines.list(limit=None)}
+        existing_extpipes = self.client.extraction_pipelines.list(limit=-1)
+
+        logging.debug(f"{existing_extpipes.as_external_ids()=}")
 
         # get requested from config
-        requested_extpipes = {
-            pipeline.external_id: ExtractionPipeline(  # key  # value
-                external_id=pipeline.external_id
-                if pipeline.external_id
-                else _render_template(self.naming_pattern, pipeline.metadata),
-                name=pipeline.name if pipeline.name else _render_template(self.naming_pattern, pipeline.metadata),
-                description=pipeline.description,
-                data_set_id=self.data_sets_in_scope.get(pipeline.data_set_external_id).id,  # type: ignore
-                raw_tables=[{"dbName": _t.db_name, "tableName": _t.table_name} for _t in pipeline.raw_tables],
-                schedule=pipeline.schedule,
-                contacts=[
-                    ExtractionPipelineContact(
-                        name=_c.name, email=_c.email, role=_c.role, send_notification=_c.send_notification
-                    )
-                    for _c in [*pipeline.contacts, *self.default_contacts]
-                ],
-                metadata=pipeline.metadata,
-                created_by=self.client._config.client_name,
-            )
-            for pipeline in self.extpipes_config.pipelines
-        }
+        requested_extpipes = ExtractionPipelineList(
+            [
+                ExtractionPipeline(  # key  # value
+                    external_id=pipeline.external_id
+                    if pipeline.external_id
+                    else _render_template(self.naming_pattern, pipeline.metadata),
+                    name=pipeline.name if pipeline.name else _render_template(self.naming_pattern, pipeline.metadata),
+                    description=pipeline.description,
+                    data_set_id=self.data_sets_in_scope.get(pipeline.data_set_external_id).id,  # type: ignore
+                    raw_tables=[{"dbName": _t.db_name, "tableName": _t.table_name} for _t in pipeline.raw_tables],
+                    schedule=pipeline.schedule,
+                    contacts=[
+                        ExtractionPipelineContact(
+                            name=_c.name, email=_c.email, role=_c.role, send_notification=_c.send_notification
+                        )
+                        for _c in [*pipeline.contacts, *self.default_contacts]
+                    ],
+                    metadata=pipeline.metadata,
+                    created_by=pipeline.created_by,
+                )
+                for pipeline in self.extpipes_config.pipelines
+            ]
+        )
+
+        logging.debug(f"{requested_extpipes.as_external_ids()=}")
+
         # Cognite SDK v6.30.1 does NOT support UPSERT (with ExtractionPipelines)
         # build 3 lists create/update/delete
-        create_extpipes = [
-            extpipe
-            for external_id, extpipe in requested_extpipes.items()
-            if external_id not in existing_extpipes.keys()
-        ]
+        create_extpipes = ExtractionPipelineList(
+            [
+                extpipe
+                for extpipe in requested_extpipes
+                if extpipe.external_id not in existing_extpipes.as_external_ids()
+            ]
+        )
 
-        update_extpipes = [
-            extpipe for external_id, extpipe in requested_extpipes.items() if external_id in existing_extpipes.keys()
-        ]
+        update_extpipes = ExtractionPipelineList(
+            [extpipe for extpipe in requested_extpipes if extpipe.external_id in existing_extpipes.as_external_ids()]
+        )
 
         delete_extpipes = [
-            external_id for external_id in existing_extpipes.keys() if external_id not in requested_extpipes.keys()
+            external_id
+            for external_id in existing_extpipes.as_external_ids()
+            if external_id not in requested_extpipes.as_external_ids()
         ]
 
         if create_extpipes:
-            logging.info(f"## Extraction pipelines to create:  {[extpipe.external_id for extpipe in create_extpipes]}")
+            logging.info(f"Extraction pipelines to create:  {create_extpipes.as_external_ids()}")
         if update_extpipes:
-            logging.info(f"## Extraction pipelines to update:  {[extpipe.external_id for extpipe in update_extpipes]}")
+            logging.info(f"Extraction pipelines to update:  {update_extpipes.as_external_ids()}")
         if self.extpipes_config.features.automatic_delete and delete_extpipes:
-            logging.info(f"## Extraction pipelines to delete:  {[extpipe.external_id for extpipe in update_extpipes]}")
+            logging.info(f"Extraction pipelines to delete:  {delete_extpipes}")
 
         if self.dry_run:
             logging.warning("Dry run detected. No changes to be applied to CDF.")
             return
 
-        logging.info("## Applying configuration")
+        logging.info("Applying configuration")
 
         self.ensure_raw_tables()
 
         if self.extpipes_config.features.automatic_delete and delete_extpipes:
-            logging.info(f"## Extraction pipelines to delete:  {delete_extpipes}")
             self.client.extraction_pipelines.delete(external_id=delete_extpipes)
+            logging.info(f"Extraction Pipelines deleted: {len(delete_extpipes)}")
 
         if create_extpipes:
             res = self.client.extraction_pipelines.create(create_extpipes)
